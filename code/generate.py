c19ca5c849afd51066c7b49e298a275c1aadde99
poo-is-coding <93828257+poo-is-coding@users.noreply.github.com>
2022-04-08 19:48:37 +0800

    2022/4/8 upload

--- code/generate.py
+++ code/generate.py
@@ -1,27 +1,27 @@
-
+""" 
 fi =  open("testcase_violent1.txt",'w')
 num = 0
-while num<59*59:
-    if not num%59 and num!=0:fi.write("\n")
+while num<60*60:
+    if not num%60 and num!=0:fi.write("\n")
     fi.write(str(int(1e9+num))+" ")
     num+=1
 fi.close()
-
-
 """
-fi =  open("testcase_violent2.txt",'w')
+
+
+""" fi =  open("testcase_violent2.txt",'w')
 for i in range(58):
     for j in range(59):
         if j%2:
-            fi.write(str(i*j))
+            fi.write(str(int(1e9+i*j)))
         else:
-            fi.write(str(1))
+            fi.write(str(int(1e9+1)))
         fi.write(" ")
     fi.write("\n")
 for i in range(59):
-    fi.write(str(1)+" ")
-fi.close()
-"""
+    fi.write(str(int(1e9+1))+" ")
+fi.close() """
+
 """ fi =  open("testcase_violent3.txt",'w')
 num = 0
 while num<59*59:
@@ -33,3 +33,26 @@ while num<59*59:
     fi.write(str(1)+" ")
     num+=1
 fi.close() """
+
+#K-regular
+k = 5 #modify the value of k to generate a testcase that is a k-regular graph 
+
+arr = [[i//2 for j in range(2*k-3)]for i in range(2,2*k-1)]
+ind = 1
+while ind < 2*k-3:
+    num = 1
+    for i in range(2*k-3):
+        arr[ind][i] = num
+        num += 1
+        num %= k+1
+        if not num:num+=1
+    ind += 2
+#print(arr)
+fi =  open("testcase_regular.txt",'w')
+for i in arr:
+    for j in i:
+        fi.write(str(j)+" ")
+    fi.write("\n")
+fi.close()
+
+
